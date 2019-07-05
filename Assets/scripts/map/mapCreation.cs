eb69ea0fc868ec1de78ed59b22742eddb50f55e2
Petr Nemec <petrnm48@gmail.com>
2019-07-05 20:23:53 +0200

    trojuhelnikova sit uz tvori mapu ale nejde nani klikat

---
+++ Assets/scripts/map/mapCreation.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapCreation : MonoBehaviour
+{
+    // Start is called before the first frame update
+        public GameObject gameTile;
+        public GameObject backGround;
+
+
+    private const float sizeOfMapBlock = 0.8F;
+    void Start(){
+        Vector3 scaleOfObject =  backGround.GetComponent<Renderer>().bounds.size; //backGround.GetComponent<Transform>().lossyScale;
+        Debug.Log($"x= " + scaleOfObject.x);
+        Debug.Log($"Y= " + scaleOfObject.y);
+        Debug.Log($"z= " + scaleOfObject.z);
+        bool changeDirectionOftile = true;
+       /* for (float y= backGround.transform.transform.position.y-8; y < scaleOfObject.y; y+=0.95f)
+        {
+            for (float x = backGround.transform.transform.position.x-8; x < scaleOfObject.x *2 * sizeOfMapBlock; x += sizeOfMapBlock)
+            {
+                if (changeDirectionOftile)
+                {
+                    Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 180));
+                    changeDirectionOftile = !changeDirectionOftile;
+                    continue;
+                }
+                else
+                {
+                    Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 0));
+                    changeDirectionOftile = !changeDirectionOftile;
+                    continue;
+                }
+
+            }
+        }*/
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+   
+}
---
+++ Assets/scripts/map/mapTileClickhandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mapTileClickhandler : MonoBehaviour
+{
+    public GameObject thisTile;
+
+    private bool ithaveBeenClicked = false;
+
+
+
+    // Start is called before the first frame update
+    /* void Start()
+     {
+
+     }*/
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit, 20.0f))
+            {
+                Debug.Log("Aspon trochu sem klikl");
+
+                if (hit.transform  != null)
+                {
+                    OnClick();
+                }
+            }
+        }
+
+
+
+    }
+   
+    private void OnClick()
+    {
+        Debug.Log("click");
+        Destroy(thisTile);
+        if (!ithaveBeenClicked)
+        {
+            Texture2D tempTexture = (Texture2D)Resources.Load("hraciPlochaPrazdna") as Texture2D;
+
+            thisTile.GetComponent<Renderer>().material.mainTexture = tempTexture;
+
+            ithaveBeenClicked = !ithaveBeenClicked;
+
+        } else{
+            Texture2D tempTexture = (Texture2D)Resources.Load("hraciPlochaOznacena") as Texture2D;
+
+            thisTile.GetComponent<Renderer>().material.mainTexture = tempTexture;
+
+            ithaveBeenClicked = !ithaveBeenClicked;
+
+        }
+        
+       
+    }
+}
