3422f5a8cd70e70f68ff9e5231f734dbaeb6e37c
Petr Nemec <petrnm48@gmail.com>
2019-07-06 12:12:35 +0200

    na blocky uz jde klikat

--- Assets/scripts/map/mapCreation.cs
+++ Assets/scripts/map/mapCreation.cs
@@ -10,32 +10,42 @@ public class mapCreation : MonoBehaviour
 
 
     private const float sizeOfMapBlock = 0.8F;
-    void Start(){
-        Vector3 scaleOfObject =  backGround.GetComponent<Renderer>().bounds.size; //backGround.GetComponent<Transform>().lossyScale;
+
+    private void Awake()
+    {
+        Vector3 scaleOfObject = backGround.GetComponent<Renderer>().bounds.size; //backGround.GetComponent<Transform>().lossyScale;
         Debug.Log($"x= " + scaleOfObject.x);
         Debug.Log($"Y= " + scaleOfObject.y);
         Debug.Log($"z= " + scaleOfObject.z);
         bool changeDirectionOftile = true;
-       /* for (float y= backGround.transform.transform.position.y-8; y < scaleOfObject.y; y+=0.95f)
+
+        for (float y = backGround.transform.transform.position.y - scaleOfObject.y; y < scaleOfObject.y; y += 0.95f)
         {
-            for (float x = backGround.transform.transform.position.x-8; x < scaleOfObject.x *2 * sizeOfMapBlock; x += sizeOfMapBlock)
+            for (float x = backGround.transform.transform.position.x - scaleOfObject.x; x < scaleOfObject.x * 2 * sizeOfMapBlock; x += sizeOfMapBlock)
             {
                 if (changeDirectionOftile)
                 {
-                    Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 180));
+
+                    var obj = Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 180));
+                    obj.transform.parent = backGround.transform;
                     changeDirectionOftile = !changeDirectionOftile;
                     continue;
                 }
                 else
                 {
-                    Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 0));
+                    var obj = Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 0));
+                    obj.transform.parent = backGround.transform; ;
+
                     changeDirectionOftile = !changeDirectionOftile;
                     continue;
                 }
 
             }
-        }*/
+        }
 
+    }
+    void Start(){
+        
     }
     // Update is called once per frame
     void Update()
--- Assets/scripts/map/mapTileClickhandler.cs
+++ Assets/scripts/map/mapTileClickhandler.cs
@@ -4,40 +4,35 @@ using UnityEngine;
 
 public class mapTileClickhandler : MonoBehaviour
 {
-    public GameObject thisTile;
+    private Camera thisTile ;
 
     private bool ithaveBeenClicked = false;
 
 
 
     // Start is called before the first frame update
-    /* void Start()
+     void Start()
      {
-
-     }*/
+      
+     }
 
     // Update is called once per frame
-    void Update()
+
+  /*  void Update()
     {
-        if (Input.GetMouseButtonDown(0)) {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 20.0f))
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("nejaky ten click");
+            RaycastHit2D hit = Physics2D.Raycast(Input.mousePosition, new Vector2 (0,0));
+            if (hit.collider != null )
             {
-                Debug.Log("Aspon trochu sem klikl");
+                Debug.Log("Player Detected");
+              
 
-                if (hit.transform  != null)
-                {
-                    OnClick();
-                }
             }
         }
-
-
-
     }
-   
+
     private void OnClick()
     {
         Debug.Log("click");
@@ -60,5 +55,5 @@ public class mapTileClickhandler : MonoBehaviour
         }
         
        
-    }
+    }*/
 }
