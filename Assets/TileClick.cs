0c466e160001fbe47108fee7a8b5ad621ecebc2c
Petr Nemec <petrnm48@gmail.com>
2019-07-17 11:00:46 +0200

    snaha o vytvoreni mechanizmu na prenos jednotek

--- Assets/TileClick.cs
+++ Assets/TileClick.cs
@@ -8,39 +8,155 @@ public class TileClick : MonoBehaviour
 
     public Sprite markedGamefield;
     public Sprite emptyGamefield;
+    private Vector2 tileCoordinates;
+
+
+
+
+    private static bool wasAnyTileClicked = false;
+    private static GameObject lastClikedTile = null;
 
     private bool ithaveBeenClicked = false;
-    private GameObject thisTile; 
+    private GameObject typeOfunitCurectlyHaving;
+
+
     private void Awake()
     {
-   
+        typeOfunitCurectlyHaving = this.gameObject.transform.GetChild(0).gameObject;
+        Debug.Log("tile child name: "+ typeOfunitCurectlyHaving.name);
     }
     void OnMouseDown()
     {
         Debug.Log("click on obejct rectangle");
         if (ithaveBeenClicked)
-        {
-            Debug.Log("loads hraciplochaPrazdna");
-          //  Sprite tempTexture = (Sprite)Resources.Load<Sprite>("Assets/sprits/textures/gameFieldTextures/hraciPlochaPrazdna.png") as Sprite;
-
-           Debug.Log(emptyGamefield);
-
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = emptyGamefield;
-
-            ithaveBeenClicked = !ithaveBeenClicked;
+        {           
+            changeToUnMark();
+            Debug.Log("x: " + this.tileCoordinates.x + " y: " + this.tileCoordinates.y);
+         
 
         }
         else
         {
-            Debug.Log("loads hraciplochaOznacena");       
-          //  Sprite tempTexture = (Sprite)Resources.Load<Sprite>("Assets/sprits/textures/gameFieldTextures/hraciPlochaOznacena.png") as Sprite;
-            Debug.Log(markedGamefield);
+            Debug.Log("co nefunguje? wasAnytileClicked" + wasAnyTileClicked.ToString() + "&&" + (lastClikedTile != null).ToString());
+            byte numberOfchecks = 0;
+            needNewCheck:
+            if (wasAnyTileClicked && lastClikedTile != null)
+            {
 
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = markedGamefield;
-          
+                //staticky buffer na ukladani dat z naposledy klikleho bloku
+                //nejednodusi metoda prenosu dat pravdepodobne
 
-            ithaveBeenClicked = !ithaveBeenClicked;
+                Debug.Log("bufferujeme na bloku X" + this.tileCoordinates.x + "y: " + this.tileCoordinates.y);
+                Debug.Log("predchoziblock " + lastClikedTile.name + " X " + lastClikedTile.transform.position.x + " y " + lastClikedTile.transform.position.y);
 
+                TileClick scriptOfLastClickedTile = lastClikedTile.GetComponent<TileClick>();
+                try
+                {
+                    this.setTypeOfunit(scriptOfLastClickedTile.transform.GetChild(0).gameObject);
+
+                }
+                catch(System.Exception e)
+                {
+                    Debug.Log("non child on current object");
+                }
+                finally
+                {
+                    scriptOfLastClickedTile.unSetUnit();
+
+                }
+
+
+            }
+            else
+            {
+                lastClikedTile = this.gameObject;
+                numberOfchecks++;
+                if(numberOfchecks > 1)
+                {
+
+                }
+                else
+                {
+                    goto needNewCheck;
+
+                }
+            }
+            lastClikedTile = null;
+            
+            changeToMarked();
         }
     }
+
+    public void changeToMarked()
+    {
+        //Debug.Log("loads hraciplochaOznacena");
+        //  Sprite tempTexture = (Sprite)Resources.Load<Sprite>("Assets/sprits/textures/gameFieldTextures/hraciPlochaOznacena.png") as Sprite;
+       // Debug.Log(markedGamefield);
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = markedGamefield;
+
+
+        this.ithaveBeenClicked = !this.ithaveBeenClicked;
+        wasAnyTileClicked = true;
+    }
+    public void changeToUnMark()
+    {
+       // Debug.Log("loads hraciplocha neoznacena");
+        //  Sprite tempTexture = (Sprite)Resources.Load<Sprite>("Assets/sprits/textures/gameFieldTextures/hraciPlochaPrazdna.png") as Sprite;
+
+       // Debug.Log(emptyGamefield);
+
+        this.gameObject.GetComponent<SpriteRenderer>().sprite = emptyGamefield;
+        this.ithaveBeenClicked = !this.ithaveBeenClicked;
+        wasAnyTileClicked = false;
+    }
+
+    public bool getWasAnyTileClicked()
+    {
+        return wasAnyTileClicked;
+    }
+    public void setWasAnyTileClicked(bool value)
+    {
+        wasAnyTileClicked = value;
+    }
+    public void setTileCoordinates(Vector2 value)
+    {
+        this.tileCoordinates = value;
+    }
+    public Vector2 getCoordinates()
+    {
+        return this.tileCoordinates;
+    }
+    //x10 y9
+    public void setTypeOfunit( GameObject unit)
+    {
+        
+        GameObject newUnit =  Instantiate(unit,this.typeOfunitCurectlyHaving.transform.position, Quaternion.Euler(0, 0, 0),this.typeOfunitCurectlyHaving.transform);
+        Destroy(typeOfunitCurectlyHaving);
+        newUnit.transform.parent = this.transform;
+        this.typeOfunitCurectlyHaving = this.gameObject.transform.GetChild(0).gameObject;
+
+        Debug.Log("name of new unit: " +newUnit.name);
+    }
+    public void unSetUnit()
+    {
+        Debug.Log("unset");
+        try
+        {
+            Destroy(this.gameObject.transform.GetChild(0).gameObject);
+
+        }
+        catch (System.Exception e)
+        {
+            Debug.Log("exeption no child object is in exitence");
+        }
+        finally
+        {
+            this.typeOfunitCurectlyHaving = Instantiate( new GameObject("empty"));
+            this.typeOfunitCurectlyHaving.transform.parent = this.transform;
+        }
+    }
+
+
+
 }
--- Assets/scripts/map/mapCreation.cs
+++ Assets/scripts/map/mapCreation.cs
@@ -7,7 +7,9 @@ public class mapCreation : MonoBehaviour
     // Start is called before the first frame update
         public GameObject gameTile;
         public GameObject backGround;
+        public GameObject testUnit;
 
+        private List<List<TileClick>> maptiles = new List<List<TileClick>>();
 
     private const float sizeOfMapBlock = 0.8F;
 
@@ -19,37 +21,65 @@ public class mapCreation : MonoBehaviour
         Debug.Log($"z= " + scaleOfObject.z);
         bool changeDirectionOftile = true;
 
+
+        int posX = 0;
+        int posY = 0;
+
         for (float y = backGround.transform.transform.position.y - scaleOfObject.y; y < scaleOfObject.y; y += 0.95f)
         {
+            posY++;
+            posX = 0;
+            List<TileClick> tileRow = new List<TileClick>();
+
             for (float x = backGround.transform.transform.position.x - scaleOfObject.x; x < scaleOfObject.x * 2 * sizeOfMapBlock; x += sizeOfMapBlock)
             {
+                posX++;
                 if (changeDirectionOftile)
                 {
 
                     var obj = Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 180));
                     obj.transform.parent = backGround.transform;
+                  
+                    TileClick myTile =(TileClick) obj.GetComponent<TileClick>();//accesing my class for the tile object
+                    myTile.setTileCoordinates(new Vector2(posX, posY));
+                    tileRow.Add(myTile);
                     changeDirectionOftile = !changeDirectionOftile;
                     continue;
                 }
                 else
                 {
                     var obj = Instantiate(gameTile, new Vector3(x * sizeOfMapBlock, y, 0), Quaternion.Euler(0, 0, 0));
-                    obj.transform.parent = backGround.transform; ;
+                    obj.transform.parent = backGround.transform;
 
+                    TileClick myTile = (TileClick)obj.GetComponent<TileClick>();//accesing my class for the tile object
+                    myTile.setTileCoordinates(new Vector2(posX, posY));
+                    tileRow.Add(myTile);
                     changeDirectionOftile = !changeDirectionOftile;
                     continue;
                 }
-
             }
+            maptiles.Add(tileRow);
         }
 
     }
     void Start(){
-        
+        for(int y=0; y < maptiles.Count; y++)
+        {
+            for(int x=0; x < maptiles[y].Count; x++)
+            {
+                if (x == 10 && y == 9)
+                {
+                    Debug.Log("x: " + maptiles[y][x].getCoordinates().x.ToString() + " y: " + maptiles[y][x].getCoordinates().y.ToString());
+                    maptiles[y][x].setTypeOfunit(testUnit);
+                }
+                
+            }
+        }
     }
     // Update is called once per frame
     void Update()
     {
+        
         
     }
    
